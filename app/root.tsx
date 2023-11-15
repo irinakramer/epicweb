9e4e249e160fc8ef7be493a2565aab7a66f0b78f
Irina Kramer <irakramer@gmail.com>
2023-11-14 16:33:44 -0800

    clean up code

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,6 @@
+import { type LinksFunction } from '@remix-run/node'
 import { LiveReload, Scripts, Links } from '@remix-run/react'
 import { KCDShop } from './kcdshop.tsx'
-import { LinksFunction } from '@remix-run/node'
-
-// 🐨 export a links function here that adds the favicon
-// 💰 It should have the following properties:
-// - rel: 'icon'
-// - type: 'image/svg+xml'
-// - href: '/favicon.svg'
 
 export const links: LinksFunction = () => {
 	return [
