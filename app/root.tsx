1749b698e90965ccb7e6d6850742e2c6b2880028
Irina Kramer <irakramer@gmail.com>
2023-11-15 19:05:57 -0800

    Style and layout updaates to make the page look nice, per EpicWeb workshop

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,20 @@
+import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
-import { type LinksFunction } from '@remix-run/node'
-import { LiveReload, Scripts, Links } from '@remix-run/react'
+import { json, type LinksFunction } from '@remix-run/node'
+import {
+	Links,
+	LiveReload,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import fontStylesheetUrl from './styles/font.css'
 import tailwindStylesheetUrl from './styles/tailwind.css'
+
+export async function loader() {
+	return json({ username: os.userInfo().username })
+}
 
 export const links: LinksFunction = () => {
 	return [
@@ -25,13 +36,35 @@ export const links: LinksFunction = () => {
 }
 
 export default function App() {
+	const data = useLoaderData<typeof loader>()
+
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
 				<Links />
 			</head>
-			<body>
-				<p className="p-8 text-xl">Hello World</p>
+			<body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
 				<Scripts />
 				<LiveReload />
 			</body>
