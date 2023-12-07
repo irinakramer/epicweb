1897cfd5b523ddaf6a47d1c2decf180ad47a0cd3
Irina Kramer <irakramer@gmail.com>
2023-12-07 14:45:30 -0800

    Added unique enforcer, other req's, catch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,31 @@
 import fs from 'node:fs'
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
+import { UniqueEnforcer } from 'enforce-unique'
 import { promiseHash } from 'remix-utils/promise'
 
 const prisma = new PrismaClient()
 
+const uniqueUsernameEnforcer = new UniqueEnforcer()
+
 export function createUser() {
 	const firstName = faker.person.firstName()
 	const lastName = faker.person.lastName()
-	const username = faker.internet.userName({
-		firstName: firstName.toLowerCase(),
-		lastName: lastName.toLowerCase(),
-	})
+
+	const username = uniqueUsernameEnforcer
+		.enforce(() => {
+			return (
+				faker.string.alphanumeric({ length: 2 }) + "_" 
+					+ faker.internet.userName({
+						firstName: firstName.toLowerCase(),
+						lastName: lastName.toLowerCase(),
+					})
+				)
+		})
+		.slice(0, 20)
+		.toLowerCase()
+		.replace(/[^a-z0-9_]/g, "_")
+
 	return {
 		username,
 		name: `${firstName} ${lastName}`,
@@ -94,31 +108,35 @@ async function seed() {
 	)
 
 	for (let index = 0; index < totalUsers; index++) {
-		await prisma.user.create({
-			data: {
-				...createUser(),
-				image: { create: userImages[index % 10] },
-				notes: {
-					create: Array.from({
-						length: faker.number.int({ min: 1, max: 3 })
-					}).map(() => ({
-						title: faker.lorem.sentence(),
-						content: faker.lorem.paragraphs(),
-						images: {
-							create: Array.from({
-								length: faker.number.int({ min: 1,max: 3 })
-							}).map(() => {
-								const imgNumber = faker.number.int({ min: 0, max: 9 })
-								return noteImages[imgNumber]
-							})
-						}
-					}))
+		await prisma.user
+			.create({
+				data: {
+					...createUser(),
+					image: { create: userImages[index % 10] },
+					notes: {
+						create: Array.from({
+							length: faker.number.int({ min: 1, max: 3 }),
+						}).map(() => ({
+							title: faker.lorem.sentence(),
+							content: faker.lorem.paragraphs(),
+							images: {
+								create: Array.from({
+									length: faker.number.int({ min: 1, max: 3 }),
+								}).map(() => {
+									const imgNumber = faker.number.int({ min: 0, max: 9 })
+									return noteImages[imgNumber]
+								}),
+							},
+						})),
+					},
 				},
-			},
-		})
+			})
+			.catch(e => {
+				console.error('Error creating a user:', e)
+				return null
+			})
 	}
 
-	
 	console.timeEnd(`👤 Created ${totalUsers} users...`)
 
 	console.time(`🐨 Created user "kody"`)
@@ -169,7 +187,7 @@ async function seed() {
 						title: 'Basic Koala Facts',
 						content:
 							'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
-							images: { create: [kodyImages.cuteKoala, kodyImages.koalaEating] },
+						images: { create: [kodyImages.cuteKoala, kodyImages.koalaEating] },
 					},
 				],
 			},
@@ -188,4 +206,3 @@ seed()
 	.finally(async () => {
 		await prisma.$disconnect()
 	})
-
