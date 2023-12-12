214c975eba233c5bfb20b6aae55af31d60c2f071
Irina Kramer <irakramer@gmail.com>
2023-12-12 14:43:23 -0800

    Migrated note and edit note to prisma, changes to image schema per epicweb-dev

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -5,28 +5,25 @@ import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { validateCSRF } from '#app/utils/csrf.server.ts'
-import { db } from '#app/utils/db.server.ts'
+import { prisma, db } from '#app/utils/db.server.ts'
 import { getNoteImgSrc, invariantResponse } from '#app/utils/misc.tsx'
 import { type loader as notesLoader } from './notes.tsx'
 
 export async function loader({ params }: DataFunctionArgs) {
-	const note = db.note.findFirst({
-		where: {
-			id: {
-				equals: params.noteId,
+	const note = await prisma.note.findFirst({
+		select: {
+			title: true,
+			content: true,
+			images: {
+				select: { id: true, altText: true },
 			},
 		},
+		where: { id: params.noteId },
 	})
 
 	invariantResponse(note, 'Note not found', { status: 404 })
 
-	return json({
-		note: {
-			title: note.title,
-			content: note.content,
-			images: note.images.map(i => ({ id: i.id, altText: i.altText })),
-		},
-	})
+	return json({ note })
 }
 
 export async function action({ request, params }: DataFunctionArgs) {
