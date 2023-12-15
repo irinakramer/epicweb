1f55b30053814274643387b89809d9d99a981ef6
Irina Kramer <irakramer@gmail.com>
2023-12-15 11:55:32 -0800

    Added nested Order by most recent note to SQL query

--- app/routes/users+/index.tsx
+++ app/routes/users+/index.tsx
@@ -24,11 +24,18 @@ export async function loader({ request }: DataFunctionArgs) {
 
 	const like = `%${searchTerm ?? ''}%`
 	const rawUsers = await prisma.$queryRaw`
-		SELECT User.id, User.username, User.name, UserImage.id as imageId
+		SELECT User.id, User.username, User.name, UserImage.id AS imageId
 		FROM User
 		LEFT JOIN UserImage ON UserImage.userId = User.id
 		WHERE User.username LIKE ${like}
 		OR User.name LIKE ${like}
+		ORDER BY (
+			SELECT Note.updatedAt
+			FROM Note
+			WHERE Note.ownerId = user.id
+			ORDER BY Note.updatedAt DESC
+			LIMIT 1
+		) DESC
 		LIMIT 50
 	`
 
